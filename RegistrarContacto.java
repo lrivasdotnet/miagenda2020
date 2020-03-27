d028fe34834bc71b38befa823efd1d0d1810006d
lrivasdotnet <luishr_itca_fepade@hotmail.com>
2020-03-27 04:11:38 -0600

    Update RegistrarContacto.java

--- RegistrarContacto.java
+++ RegistrarContacto.java
@@ -1,12 +1,15 @@
 package net.lrivas.miagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import net.lrivas.miagenda.clases.ConexionSQLite;
 
 public class RegistrarContacto extends AppCompatActivity {
@@ -20,41 +23,45 @@ public class RegistrarContacto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_contacto);
         objConexion = new ConexionSQLite(RegistrarContacto.this,NOMBRE_BASE_DATOS,null,1);
-        nombre = (EditText) findViewById(R.id.txtNombreCompleto);
-        telefono = (EditText) findViewById(R.id.txtTelefono);
-        botonAgregar = (Button) findViewById(R.id.btnGuardarContacto);
-        botonRegresar = (Button) findViewById(R.id.btnRegresar);
-        botonRegresar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                regresar();
-            }
-        });
 
+        nombre = findViewById(R.id.txtNombreCompleto);
+        telefono = findViewById(R.id.txtTelefono);
+        botonAgregar = findViewById(R.id.btnGuardarContacto);
+        botonRegresar = findViewById(R.id.btnRegresar);
+
+        //Evento (CLICK)
         botonAgregar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                //Registrar
                 registrar();
             }
         });
+
+        botonRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Regresar
+                regresar();
+            }
+        });
     }
 
-    private void registrar() {
-        try{
-            SQLiteDatabase miBaseDatos = objConexion.getWritableDatabase();
-            String comando = "INSERT INTO contactos (nombre,telefono) VALUES " +
-                    "('"+ nombre.getText() +"','"+ telefono.getText() +"')";
-            miBaseDatos.execSQL(comando);
-            miBaseDatos.close();
-            Toast.makeText(RegistrarContacto.this, "Datos Registrados con exito", Toast.LENGTH_SHORT).show();
-        }catch (Exception error){
-            Toast.makeText(RegistrarContacto.this, "Error: "+ error.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void regresar() {
-        Intent actividad = new Intent(RegistrarContacto.this, MainActivity.class);
+    private void regresar(){
+        Intent actividad = new Intent(RegistrarContacto.this,MainActivity.class);
         startActivity(actividad);
         RegistrarContacto.this.finish();
+    }
+
+    private void registrar(){
+        try{
+            SQLiteDatabase miBaseDatos = objConexion.getWritableDatabase();
+            String comando = "INSERT INTO contactos (nombre,telefono) VALUES ('"+ nombre.getText() +"','"+ telefono.getText() +"')";
+            miBaseDatos.execSQL(comando);
+            miBaseDatos.close();
+            Toast.makeText(RegistrarContacto.this, "Datos Registrados con éxito", Toast.LENGTH_LONG).show();
+        }catch (Exception error){
+            Toast.makeText(RegistrarContacto.this, "Error: "+ error.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
